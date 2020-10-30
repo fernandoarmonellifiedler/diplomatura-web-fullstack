aa105768ea123fa3cd88b2193e30428508b39b54
fernandoarmonellifiedler <62020070+fernandoarmonelli@users.noreply.github.com>
2020-10-30 18:30:27 -0300

    working a little bit more

--- Ejercicios/modulo-2/unidad-3/06_trabajo-practico/script.js
+++ Ejercicios/modulo-2/unidad-3/06_trabajo-practico/script.js
@@ -1,17 +1,23 @@
 /* Trabajo Práctico:
-Modificar la tabla de meses desarrollada en la Unidad para que al recargar (o cargar por primera vez) la página, la tabla se empiece a dibujar de arriba hacia abajo con un movimiento perceptible (dar un tiempo entre el dibujo de una fila y la siguiente para que se perciba el movimiento). */
+Modificar la tabla de meses desarrollada en la Unidad para que al recargar (o cargar por primera vez) la página, la tabla se empiece a dibujar de arriba hacia abajo con un movimiento perceptible (dar un tiempo entre el dibujo de una fila y la siguiente para que se perciba el movimiento).
+
+Notas:
+- para poder darle un intervalo a la creacion de cada fila preferi recrear la tabla con js. 
+- de este modo el intervalo se agregó dentro de la funcion dibujar() en ciclo donde se crearon las filas
+*/
 
 var meses = ['enero','febrero','marzo','abril','mayo','junio','julio','agosto','septiembre','octubre','noviembre','diciembre'];
 var dias = [31,28,31,30,31,30,31,31,30,31,30,31];
 
+// titulo de la pagina
 var titulo = document.querySelector("h1");
 titulo.style.color="green";
-
+// tabla
 var divTabla = document.getElementById("div-tabla");
 
 var tabla = document.createElement("table");
 divTabla.appendChild(tabla);
-    
+// filas iniciales (Mes - Dias)    
 var filaInicial = document.createElement("tr");
 tabla.appendChild(filaInicial);
     
@@ -26,8 +32,7 @@ colDiasInicial.appendChild(colDiasInicialTexto);
 filaInicial.appendChild(colMesInicial);
 filaInicial.appendChild(colDiasInicial);
 
-
-
+// Funcion que crea cada una de las filas al interior de la tabla teniendo en cuenta el intervalo de tiempo insertado
 function dibujar() {
     var i = 0;
     var recorrido = setInterval(() => {
@@ -54,80 +59,46 @@ function dibujar() {
     }, 100);
 }
 
+// seccion para los botones
+var fila = document.getElementsByTagName("tr");
 var divBotonera = document.getElementById("botonera");
 
+// boton Modo Nocturno
 var botonNocturno = document.createElement("button");
 var botonNocturnoTexto = document.createTextNode("Nocturno");
 botonNocturno.appendChild(botonNocturnoTexto);
 divBotonera.appendChild(botonNocturno);
+
 botonNocturno.addEventListener("click", function() {
     document.body.style.backgroundColor = "darkslategrey";
 
-    for (let i = 1 ; i < meses.length ; i++) {
-        tabla[i].style.color = "white";
+    for (let i = 1 ; i <= meses.length ; i++) {
+        fila[i].style.color = "white";
 
         if (i%2 == 0) {
-            tabla[i].style.backgroundColor = "grey";
+            fila[i].style.backgroundColor = "grey";
         } else {
-            tabla[i].style.backgroundColor = "black";
+            fila[i].style.backgroundColor = "black";
         }
     }
 })
 
+// boton Modo Diurno
 var botonDiurno = document.createElement("button");
 var botonDiurnoTexto = document.createTextNode("Diurno");
 botonDiurno.appendChild(botonDiurnoTexto);
 divBotonera.appendChild(botonDiurno);
+
 botonDiurno.addEventListener("click", function() {
     document.body.style.backgroundColor = "lightslategrey";
     
-    for (let i = 1 ; i < meses.length ; i++) {
-        tabla[i].style.color = "black";
+    for (let i = 1 ; i <= meses.length ; i++) {
+        fila[i].style.color = "black";
 
         if (i%2 == 0) {
-            tabla[i].style.backgroundColor = "palevioletred";
+            fila[i].style.backgroundColor = "palevioletred";
         } else {
-            tabla[i].style.backgroundColor = "pink";
+            fila[i].style.backgroundColor = "pink";
         }
     }
 } )
-
-/* 
-var divBotonera = document.getElementById("botonera");
-
-var botonNocturno = document.createElement("button");
-var botonNocturnoTexto = document.createTextNode("Nocturno");
-botonNocturno.appendChild(botonNocturnoTexto);
-divBotonera.appendChild(botonNocturno);
-botonNocturno.addEventListener("click", function() {
-    document.body.style.backgroundColor = "darkslategrey";
-
-    for (let i = 1 ; i < meses.length ; i++) {
-        tabla[i].style.color = "white";
-
-        if (i%2 == 0) {
-            tabla[i].style.backgroundColor = "grey";
-        } else {
-            tabla[i].style.backgroundColor = "black";
-        }
-    }
-})
-
-var botonDiurno = document.createElement("button");
-var botonDiurnoTexto = document.createTextNode("Diurno");
-botonDiurno.appendChild(botonDiurnoTexto);
-divBotonera.appendChild(botonDiurno);
-botonDiurno.addEventListener("click", function() {
-    document.body.style.backgroundColor = "lightslategrey";
-    
-    for (let i = 1 ; i < meses.length ; i++) {
-        tabla[i].style.color = "black";
-
-        if (i%2 == 0) {
-            tabla[i].style.backgroundColor = "palevioletred";
-        } else {
-            tabla[i].style.backgroundColor = "pink";
-        }
-    }
-} )
-*/
