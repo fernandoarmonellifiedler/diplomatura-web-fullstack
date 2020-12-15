142c823ab592806d55d754c8baa4cc5953a35f00
fernandoarmonellifiedler <fernandoarmonelli@gmail.com>
2020-12-15 17:42:39 -0300

    add handlebars tutorial explanation

--- Ejercicios/modulo-4/unidad-1/05_notas-sobre-ejemplo/00_intro.js
+++ Ejercicios/modulo-4/unidad-1/05_notas-sobre-ejemplo/00_intro.js
@@ -6,6 +6,10 @@ File documentation on this folder:
 01) All about MongoDB and Mongoose
 02) About Express handlebars
 03) Body-parser
+04) Express-handlebar
+
+turn off funds on npm:
+npm config set fund false 
 */
 
 /* ===== index.js file ===== */
@@ -29,6 +33,9 @@ mongoose.connect('mongodb://localhost/curso_nodejs_intermedio_m1u2', {}).then( (
 
 /* ----- Body-parser ----- */
 var body_parser = require("body-parser");
+// La informacion enviada por POST de un formulario se recibe en req.body
+app.use(body_parser.urlencoded({extended: false}));
+app.use(body_parser.json());
 
 /* ----- Express-handlebars ----- */
 var handlebars = require('express-handlebars')
@@ -36,5 +43,30 @@ var handlebars = require('express-handlebars')
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
+// ---------------------------------------
+// Funcion que se va a ejecutar en cada peticion
+var myLogger = function(req, res, next) {
+    console.log('Paso por el logger');
+    next();
+}
+
+// Funcion handler de errores
+var myErrorHandler = function(error, req, res, next) {
+    console.log('Paso por el manejador de errores');
+    console.log(error.message); // Mensaje de error
+    res.status(error.status || 500); // Retornamos el estado del error o 500 si no tiene estado
+    res.send(error.message);
+}
+
+app.use(myLogger);
 
 // ---------------------------------------
+// Primer uso de handlebars
+app.get('/', function (request, response) {
+    response.render('principal'); // views/principal.handlebars
+    //response.send('Bienvenido al curso de NodeJS nivel Intermedio!');
+});
+
+// views/principal.handlebars esta renderizado en la respuesta al acceso a la carpeta raiz
+//<h2>Principal</h2>
+//<p>Un texto que acompaña a la página principal</p>
