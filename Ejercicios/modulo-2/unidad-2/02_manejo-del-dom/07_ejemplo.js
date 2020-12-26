7de96efa4a4aee6acb0db98413397f4e686a94e1
fernandoarmonellifiedler <fernandoarmonelli@gmail.com>
2020-12-26 16:32:28 -0300

    reorg folders

--- Ejercicios/modulo-2/unidad-2/02_manejo-del-dom/07_ejemplo.js
+++
@@ -1,7 +0,0 @@
-/* Desarrollamos un programa que muestre una tabla con los meses del año y los días que tiene cada mes. Con 2 botones, vamos a cambiar el estilo de la tabla.
-
-- Al  cargar la página se muestra una tabla.
-- Al hacer clic en el botón “Nocturno” debe cambiar el estilo.
-- Al hacer clic en el botón “Diurno” debe cambiar el estilo.
-
-Sigue el código en la proxima carpeta:*/
--- Ejercicios/modulo-2/unidad-2/03_practicando-con-DOM/01_un-ejemplo/script.js
+++
@@ -1,4 +0,0 @@
-function setBodyAttr(attr, value) {
-    if (document.body) document.body[attr] = value;
-    else throw new Error("no support");
-  }
