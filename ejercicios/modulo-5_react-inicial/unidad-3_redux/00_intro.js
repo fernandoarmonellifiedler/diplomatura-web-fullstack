0b66c6d9637b53e6ed5346a847584ff577e7b094
fernandoarmonellifiedler <fernandoarmonelli@gmail.com>
2021-02-01 14:13:53 -0300

    update files

---
+++ ejercicios/modulo-5_react-inicial/unidad-3_redux/00_intro.js
@@ -0,0 +1,13 @@
+/* Redux 
+
+Redux es un patrón de arquitectura que permite persistir datos en todo el proyecto ReactJS, reduciendo la cantidad de relaciones entre los componentes. Es fundamental
+conocerlo y manejarlo al trabajar profesionalmente en ReactJS
+
+Bloques temáticos:
+1. Concepto de redux
+2. Ventajas del uso de redux
+3. Uso de redux en un proyecto React
+4. Implementación de redux en React
+5. Ejemplo
+6. Trabajo Práctico
+*/
---
+++ ejercicios/modulo-5_react-inicial/unidad-3_redux/02_redux-en-react.js
@@ -0,0 +1,25 @@
+/* Uso de redux en un proyecto React
+Pasos en React
+Comunicación componente - store
+Centro de e-Learning SCEU UTN - BA.
+Medrano 951 2do piso (1179) // Tel. +54 11 4867 7589 / Fax +54 11 4032 0148
+www.sceu.frba.utn.edu.ar/e-learning
+p. 14
+4. Implementación de redux en React
+Paso 1: Instalación
+Paso 2: Crear reducer
+src/ContadorReducer.js
+Paso 3: Componente inicial
+index.js
+Centro de e-Learning SCEU UTN - BA.
+Medrano 951 2do piso (1179) // Tel. +54 11 4867 7589 / Fax +54 11 4032 0148
+www.sceu.frba.utn.edu.ar/e-learning
+p. 15
+Paso 4: Agregamos las funciones de mapeo y dispatch
+Centro de e-Learning SCEU UTN - BA.
+Medrano 951 2do piso (1179) // Tel. +54 11 4867 7589 / Fax +54 11 4032 0148
+www.sceu.frba.utn.edu.ar/e-learning
+p. 16
+5. Ejemplo
+Contador en React con Redux
+https://github.com/cursos-utn/react_ejemplos/tree/master/06-redux-contador*/
