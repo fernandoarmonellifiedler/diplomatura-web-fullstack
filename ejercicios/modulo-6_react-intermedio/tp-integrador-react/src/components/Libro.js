359a26edd24ef968891000b403494620780122b6
fernandoarmonellifiedler <fernandoarmonelli@gmail.com>
2021-02-22 21:18:16 -0300

    update libro.js - working

--- ejercicios/modulo-6_react-intermedio/tp-integrador-react/src/components/Libro.js
+++ ejercicios/modulo-6_react-intermedio/tp-integrador-react/src/components/Libro.js
@@ -1,12 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
+// uuid module (unique id)
+// import { v4 as uuidv4 } from 'uuid';
+// uuidv4(); // ⇨ '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d'
+
 const Libro = () => {
-  // lista de libros
-  const [libros, setLibros] = useState([]);
   // nombre y descripcion de libro nuevo
   const [nombre, setNombre] = useState('');
   const [descripcion, setDescripcion] = useState('');
+  const [categoria, setCategoria] = useState('');
+  const [persona, setPersona] = useState('');
+
+  // lista de libros
+  const [libros, setLibros] = useState([]);
 
   useEffect(async () => {
     try {
@@ -21,12 +28,22 @@ const Libro = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
     if (nombre && descripcion) {
-      const nuevoLibro = { id: '', nombre, descripcion };
+      const nuevoLibro = {
+        id: new Date().getTime().toString(),
+        nombre,
+        descripcion,
+        categoria,
+        persona,
+      };
+      console.log(nuevoLibro);
       setLibros((libros) => {
         return [...libros, nuevoLibro];
       });
+      // falta comando en axios para agregar el libro nuevo a la base de datos MySQL
       setNombre('');
       setDescripcion('');
+      setCategoria('');
+      setPersona('');
     } else {
       window.alert('No puedes ingresar valores en blanco');
     }
@@ -62,17 +79,46 @@ const Libro = () => {
               onChange={(e) => setDescripcion(e.target.value)}
             />
           </div>
+          {/* Categoria */}
+          <div className='form-control'>
+            <label htmlFor='libro-categoria'>Categoria: </label>
+            <input
+              type='text'
+              id='libro-categoria'
+              name='libro-categoria'
+              value={categoria}
+              onChange={(e) => setCategoria(e.target.value)}
+            />
+          </div>
+          {/* Persona */}
+          <div className='form-control'>
+            <label htmlFor='libro-persona'>Persona: </label>
+            <input
+              type='text'
+              id='libro-persona'
+              name='libro-persona'
+              value={persona}
+              onChange={(e) => setPersona(e.target.value)}
+            />
+          </div>
           <button type='submit'>Agregar Libro</button>
         </form>
 
         <h2>Listado de libros</h2>
         {libros.map((unLibro) => {
+          const {
+            id,
+            nombre_libro,
+            descripcion,
+            categoria_id,
+            persona_id,
+          } = unLibro;
           return (
-            <div className='item' key={unLibro.id}>
-              <h4>{unLibro.nombre_libro}</h4>
-              <p>{unLibro.descripcion}</p>
-              <p>{unLibro.categoria_id}</p>
-              <p>{unLibro.persona_id}</p>
+            <div className='item' key={id}>
+              <h4>{nombre_libro || 'sin libro'}</h4>
+              <p>{descripcion || 'sin descripcion'}</p>
+              <p>{categoria_id || 'sin categoria'}</p>
+              <p>{persona_id || 'libro disponible'}</p>
               <button className='btn'>Editar</button>
               <button className='btn'>Eliminar</button>
               <button className='btn'>prestar / devolver</button>
